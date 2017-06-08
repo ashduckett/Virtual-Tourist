7b518b600f20620f1a3448448ed73ac2a2770666
Ash Duckett <ashduckett@Ashs-iMac.local>
2017-06-08 02:05:22 +0100

    Pins now show on press and hold of map

--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
+        
+        // Ensure that we add an annotation when the user presses on the map for 1.5 seconds
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation))
+        longPress.minimumPressDuration = 1.5
+        mapView.addGestureRecognizer(longPress)
+    }
+    
+    func addAnnotation(gestureRecogniser: UIGestureRecognizer) {
+        if gestureRecogniser.state == .began {
+            let touchPoint = gestureRecogniser.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    // I use this to describe the look of each annotation
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .purple
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
